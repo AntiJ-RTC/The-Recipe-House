ca6ae6354628c263ead364c6242324b9000e10c8
SEPTS7N <SEPTS7N@SEPT_LAPTOP>
2024-06-10 21:34:37 -0700

    Add project files.

---
+++ Models/Comments.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Recipe_House.Models
+{
+    public class Comments
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; }
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,12 @@
+﻿namespace The_Recipe_House.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+
+    }
+}
